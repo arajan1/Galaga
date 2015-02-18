dc18641e9f0b62720178ef3e6b7329981db93540
ginge876 <pdickinson@exeter.edu>
2015-02-18 14:00:56 -0500

    Update GUI.java

--- src/GUI.java
+++ src/GUI.java
@@ -1,6 +1,42 @@
+//import java.util.*;
+//import java.awt.*;
+import java.awt.event.*;
 
-public class GUI {
-	public static void main(String[] args){
+public class GUI implements KeyListener
+{
+	public static void main (String[] args)
+	{
+		GUI test = new GUI();    
+	}
+	
+	public GUI()
+	{
 		
 	}
+
+	@Override
+	public void keyPressed(KeyEvent evt) 
+	{
+		int kp = evt.getKeyCode();
+		switch(kp)
+		{
+			case KeyEvent.VK_SPACE: 
+				System.out.println("test");
+				break;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) 
+	{
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) 
+	{
+		
+	}
+	
+	
 }
