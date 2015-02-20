f2ffabde73e855c9984f834b544a3e103b974187
ginge876 <pdickinson@exeter.edu>
2015-02-20 09:11:20 -0500

    Update Objects.java

--- src/Objects.java
+++ src/Objects.java
@@ -1,43 +1,55 @@
-
-public abstract class Objects {
+public abstract class Objects 
+{
 	int x = 0;
 	int y = 0;
 	int width = 0;
 	int height = 0;
-	Objects(int a,int b,int c,int d){
+	Objects(int a,int b,int c,int d)
+	{
 		x = a;
 		y = b;
 		width = c;
 		height = d;
 	}
-	public int getX(){
+	public int getX()
+	{
 		return x;
 	}
-	public int getY(){
+	public int getY()
+	{
 		return y;
 	}
-	public int getWidth(){
+	public int getWidth()
+	{
 		return width;
 	}
-	public int getHeight(){
+	public int getHeight()
+	{
 		return height;
 	}
-	public void setX(int a){
+	public void setX(int a)
+	{
 		x = a;
 	}
-	public void setY(int a){
+	public void setY(int a)
+	{
 		y = a;
 	}
-	public void changeX(int a){
+	public void changeX(int a)
+	{
 		x += a;
 	}
-	public void changeY(int a){
+	public void changeY(int a)
+	{
 		y += a;
 	}
-	public void changeNegX(int a){
+	public void changeNegX(int a)
+	{
 		x -= a;
 	}
-	public void changeNegY(int a){
+	public void changeNegY(int a)
+	{
 		y -= a;
 	}
 }
+
