09c6d24e6c8347db4a21c16c1709d9e0881b67c4
ginge876 <pdickinson@exeter.edu>
2015-02-20 09:17:50 -0500

    Update BulletList.java

--- src/BulletList.java
+++ src/BulletList.java
@@ -1,36 +1,42 @@
 
 
-public class BulletList {
+public class BulletList 
+{
 	private Link<Bullet> head; // Pointer to list header
 	private Link<Bullet> tail; // Pointer to last element
 	protected Link<Bullet> curr; // Access to current element
 	int cnt; // Size of list
 
 	/** Constructors */
-	BulletList(int size) {
+	BulletList(int size) 
+	{
 		this();
 	} // Constructor -- Ignore size
-
-	BulletList() {
+	//why bother with this constructer^?
+	BulletList() 
+	{
 		curr = tail = head = new Link<Bullet>(null); // Create header
 		cnt = 0;
 	}
 
 	/** Remove all elements */
-	public void clear() {
+	public void clear() 
+	{
 		head.setNext(null); // Drop access to links
 		curr = tail = head = new Link<Bullet>(null); // Create header
 		cnt = 0;
 	}
 
 	/** Append "it" to list */
-	public void append(Bullet it) {
+	public void append(Bullet it) 
+	{
 		tail = tail.setNext(new Link<Bullet>(it, null));
 		cnt++;
 	}
 
 	/** Remove and return current element */
-	public Bullet remove() {
+	public Bullet remove() 
+	{
 		if (curr.next() == null)
 			return null; // Nothing to remove
 		Bullet it = curr.next().element(); // Remember value
@@ -42,17 +48,20 @@ public class BulletList {
 	}
 
 	/** Set curr at list start */
-	public void moveToStart() {
+	public void moveToStart() 
+	{
 		curr = head;
 	}
 
 	/** Set curr at list end */
-	public void moveToEnd() {
+	public void moveToEnd() 
+	{
 		curr = tail;
 	}
 
 	/** Move curr one step left; no change if now at front */
-	public void prev() {
+	public void prev() 
+	{
 		if (curr == head)
 			return; // No previous element
 		Link<Bullet> temp = head;
@@ -63,23 +72,27 @@ public class BulletList {
 	}
 
 	/** Move curr one step right; no change if now at end */
-	public void next() {
+	public void next() 
+	{
 		if (curr != tail)
 			curr = curr.next();
 	}
 
 	/** @return List length */
-	public int length() {
+	public int length() 
+	{
 		return cnt;
 	}
 
-	public Bullet getValue() {
+	public Bullet getValue() 
+	{
 		if (curr.next() == null)
 			return null;
 		return (Bullet) curr.next().element();
 	}
 	
-	public void traverseListPlayer(){
+	public void traverseListPlayer()
+	{
 		moveToStart();
 		for(int i = 0; i < length(); i++){
 			if(getValue()!=null){
@@ -94,21 +107,25 @@ public class BulletList {
 	}
 }
 
-class Link<Bullet> {
+class Link<Bullet> 
+{
 	private Bullet element; // Value for this node
 	private Link<Bullet> next; // Pointer to next node in list
 
 	// Constructors
-	Link(Bullet it, Link<Bullet> nextval) {
+	Link(Bullet it, Link<Bullet> nextval) 
+	{
 		element = it;
 		next = nextval;
 	}
 
-	Link(Link<Bullet> nextval) {
+	Link(Link<Bullet> nextval) 
+	{
 		next = nextval;
 	}
 
-	Link<Bullet> next() {
+	Link<Bullet> next() 
+	{
 		return next;
 	} // Return next field
 
@@ -117,11 +134,13 @@ class Link<Bullet> {
 		return next = nextval;
 	} // Return element field
 
-	Bullet element() {
+	Bullet element() 
+	{
 		return element;
 	} // Set element field
 
-	Bullet setElement(Bullet it) {
+	Bullet setElement(Bullet it) 
+	{
 		return element = it;
 	}
 }
