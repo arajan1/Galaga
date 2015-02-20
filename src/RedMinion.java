cb5e0adc094166193466b81735f3a4a4c8162907
arajan1 <arajan1@exeter.edu>
2015-02-20 10:52:36 -0500

    test

--- src/RedMinion.java
+++ src/RedMinion.java
@@ -1,4 +1,30 @@
 
-public class RedMinion {
+public class RedMinion extends Objects implements Monster{
+
+	RedMinion(int a, int b, int c, int d) {
+		super(a, b, c, d);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public void move() {
+		
+	}
+
+	public void enter() {
+		
+	}
+
+	public void attack() {
+		
+	}
+
+	public void moveBackandFowarth() {
+		
+	}
+
+	public void returnToTop() {
+		
+	}
 
 }
