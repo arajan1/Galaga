d200d1a3fc01f2b7a6fe2aeee4b7619092cf6da3
ginge876 <pdickinson@exeter.edu>
2015-02-23 12:52:43 -0500

    Update and rename src to src/Sprite.java

---
+++ src/Sprite.java
@@ -0,0 +1,35 @@
+//WORK IN PROGRESS
+
+package galagaProject;
+
+import java.awt.Image;
+
+public class Sprite 
+{
+	int angle;//so sprite can be rotated according to velocity
+	int x;
+	int y;
+	Image fileName;
+	
+	public int getAngle()
+	{
+		return 0;
+	}
+	public int getX()
+	{
+		return x;
+	}
+	public int getY()
+	{
+		return y;
+	}
+	public Image getFileName()
+	{
+		return fileName;
+	}
+	public void display(Graphics)
+	{
+		
+	}
+	
+}
