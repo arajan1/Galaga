ab0d760bd1070b35a8f9a911b0b1b74a4275e196
ginge876 <pdickinson@exeter.edu>
2015-02-23 12:53:49 -0500

    Update and rename src to src/GamePanel.java

---
+++ src/GamePanel.java
@@ -0,0 +1,34 @@
+//WORK IN PROGRESS - DONT TOUCH
+
+package galagaProject;
+import java.util.LinkedList;
+import java.awt.*;
+
+import javax.swing.*;
+
+
+public class GamePanel extends JPanel
+{
+	LinkedList<Sprite> sprites = new LinkedList<Sprite>();
+	
+	
+	public void draw()
+	{
+		for (Sprite s: sprites)
+		{
+			
+		}
+	}
+	
+	@Override
+	public void paint(Graphics g)
+	{
+		
+	}
+	
+	protected void paintComponent(Graphics g, int x, int y, int rotation)//warning: pseudo-code ahead!!
+	{
+		super.paintComponent(g)
+	}
+	
+}
