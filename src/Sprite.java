f23d7fa96a4a288a025952ebc9965b5d4ae86e56
ginge876 <pdickinson@exeter.edu>
2015-02-26 20:54:11 -0500

    Update Sprite.java

--- src/Sprite.java
+++ src/Sprite.java
@@ -1,7 +1,11 @@
+package galagaProjectTemp;
+
 import java.awt.Graphics2D;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.geom.AffineTransform;
+
+import javax.swing.ImageIcon;
 
 public class Sprite 
 {
@@ -9,22 +13,26 @@ public class Sprite
 	int x;
 	int y;
 	Image img;
-	public Sprite()
+	public Sprite(Image im, int xc, int yc)
 	{
+		img = im;
 		angle = 0f;
+		x = xc;
+		y = yc;
 	}
 	
-	public Image getImage()
-	{
-		return img;
-	}
+	//public Image getImage()
+	//{
+		//return img;
+	//}
 	public void display(Graphics g, GamePanel gp)
 	{
 		AffineTransform at = new AffineTransform();
-		at.setToRotation(Math.toRadians(angle));
-		at.translate(x, y);
+		
+		at.setToRotation(angle);
+		((Graphics2D)g).translate(x, y);
 		((Graphics2D)g).drawImage(img, at, gp);
+		((Graphics2D)g).translate(0-x,0-y);
 	}
 	
 }
-
